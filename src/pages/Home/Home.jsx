5429fcbe6e2900691460d0af5ee1d25fa8d7f6cd
jhimi01 <talukderjhimi@gmail.com>
2025-01-13 19:49:32 +0600

    feat: add navigation and banner setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,24 @@
 const Home = () => {
   return (
     <div
-      className="text-white"
+      className="text-white relative  flex items-center"
       style={{
-        backgroundImage: `url(${"https://www.qatarairways.com/content/dam/images/renditions/horizontal-1/campaigns/global/grsp-2025/dream-destinations/hn-dream-destination-city.jpg"})`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url("https://www.qatarairways.com/content/dam/images/renditions/horizontal-1/campaigns/global/grsp-2025/dream-destinations/hn-dream-destination-city.jpg")`,
+
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
-        height: "90vh",
+        height: "78vh",
         width: "100%",
       }}
     >
-      <div className="wrapper">
-        <h2 className="text-2xl">Dream destinations meet perfect offers</h2>
-        <p>Save up to 20%*</p>
-        <button className="border border-white rounded-full px-5 py-3">
+      {/* <div
+        className="absolute inset-0 bg-black opacity-50"
+      ></div> */}
+      <div className="wrapper space-y-8">
+        <h2 className="text-5xl">Dream destinations meet perfect offers</h2>
+        <p className="text-lg">Save up to 20%*</p>
+        <button className="border border-white font-semibold rounded-full px-5 py-3">
           Book now
         </button>
       </div>
